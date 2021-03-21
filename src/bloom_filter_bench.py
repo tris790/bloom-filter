77bbe9839412121e55254558671d100ee09e528d
Tristan <tris790@gmail.com>
2021-03-20 21:52:26 -0400

    more accurate timing

--- src/bloom_filter_bench.py
+++ src/bloom_filter_bench.py
@@ -2,7 +2,6 @@ import bloom_filter
 import tracemalloc
 import time
 
-
 print("---------------- Bloom filters benchmark ----------------")
 
 words = []
@@ -11,18 +10,29 @@ with open("words.txt") as f:
 print(f"Loaded: {len(words)} words")
 
 tracemalloc.start()
-start_time = time.time_ns()
 print("Creating the bloom filter data structure")
-bf = bloom_filter.BloomFilter(64, 5)
+start_time = time.time_ns()
+bf = bloom_filter.BloomFilter(64, 3)
 
 print(f"Adding {len(words)} elements")
 for word in words:
     bf.add(word)
 
+end_time = time.time_ns()
+elapsed_time = end_time - start_time
+print(f"Time to add all elements: {elapsed_time}ns")
+
+print(f"Looking up every element")
+start_time = time.time_ns()
+for word in words:
+    if bf.exists(word) == False:
+        print(f"Error: {word} wasn't found")
+
+end_time = time.time_ns()
+elapsed_time = end_time - start_time
+print(f"Time to know if the elements were present: {elapsed_time}ns")
+
 current, peak = tracemalloc.get_traced_memory()
 print(
     f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
 tracemalloc.stop()
-end_time = time.time_ns()
-elapsed_time = end_time - start_time
-print(f"Total time: {elapsed_time}ns")
--- src/set_bench.py
+++ src/set_bench.py
@@ -9,18 +9,28 @@ with open("words.txt") as f:
 print(f"Loaded: {len(words)} words")
 
 tracemalloc.start()
-start_time = time.time_ns()
 print("Creating the set data structure")
+start_time = time.time_ns()
 set_struct = set([])
 
 print(f"Adding {len(words)} elements")
 for word in words:
     set_struct.add(word)
 
+end_time = time.time_ns()
+elapsed_time = end_time - start_time
+print(f"Time to add all elements: {elapsed_time}ns")
+
+start_time = time.time_ns()
+for word in words:
+    if word not in set_struct:
+        print(f"Error: {word} wasn't found")
+
+end_time = time.time_ns()
+elapsed_time = end_time - start_time
+print(f"Time to know if the elements were present: {elapsed_time}ns")
+
 current, peak = tracemalloc.get_traced_memory()
 print(
     f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
 tracemalloc.stop()
-end_time = time.time_ns()
-elapsed_time = end_time - start_time
-print(f"Total time: {elapsed_time}ns")
